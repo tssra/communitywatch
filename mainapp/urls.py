368d637fdbe464b6bfdca1270ff755a2b63e30ec
diwasblack <diwasblack@gmail.com>
2014-12-18 11:32:53 +0545

    Base template

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import patterns, url
 from mainapp import views
 
 urlpatterns = patterns('',
-url(r'^$', views.index, name='index'),
+	url(r'^$', views.index, name='index'),
 )
