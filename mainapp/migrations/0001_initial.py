1a5f267f4271f138091f3394815d9b9185bf2cc6
diwasblack <diwasblack@gmail.com>
2014-12-18 12:29:34 +0545

    Modified index and base file design

---
+++ mainapp/migrations/0001_initial.py
@@ -0,0 +1,69 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+from django.conf import settings
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Comment',
+            fields=[
+                ('id', models.AutoField(auto_created=True, serialize=False, verbose_name='ID', primary_key=True)),
+                ('description', models.TextField()),
+                ('senti', models.IntegerField()),
+                ('is_anonymous', models.BooleanField(default=True)),
+                ('created_at', models.DateTimeField(auto_now_add=True)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Story',
+            fields=[
+                ('id', models.AutoField(auto_created=True, serialize=False, verbose_name='ID', primary_key=True)),
+                ('title', models.CharField(max_length=255)),
+                ('description', models.TextField()),
+                ('sector', models.CharField(max_length=50)),
+                ('status', models.CharField(max_length=50)),
+                ('location', models.CharField(max_length=50)),
+                ('senti', models.IntegerField()),
+                ('created_at', models.DateTimeField(auto_now_add=True)),
+                ('user', models.ForeignKey(to=settings.AUTH_USER_MODEL)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Watch',
+            fields=[
+                ('id', models.AutoField(auto_created=True, serialize=False, verbose_name='ID', primary_key=True)),
+                ('created_at', models.DateTimeField(auto_now_add=True)),
+                ('story', models.ForeignKey(to='mainapp.Story')),
+                ('user', models.ForeignKey(to=settings.AUTH_USER_MODEL)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.AddField(
+            model_name='comment',
+            name='story',
+            field=models.ForeignKey(to='mainapp.Story'),
+            preserve_default=True,
+        ),
+        migrations.AddField(
+            model_name='comment',
+            name='user',
+            field=models.ForeignKey(to=settings.AUTH_USER_MODEL),
+            preserve_default=True,
+        ),
+    ]
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,30 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-import os
-
-scores = {}
 
 # Create your views here.
 def index(request):
-	return render(request,'mainapp/base.html',)
-
-def calcScore(text):
-	tot = 0
-	words = text.split(" ")
-	words_scores = [scores[word] for word in words if word in scores]
-	tot = sum(words_scores)
-	return tot
-
-def senti(request):
-	module_dir = os.path.dirname(__file__)  # get current directory
-	file_path = os.path.join(module_dir, 'static/mainapp/AFINN-111.txt')
-	# return HttpResponse(file_path)
-	afinnfile = open(file_path)
-    
-	for line in afinnfile:
-		term, score  = line.split("\t")  # The file is tab-delimited. "\t" means "tab character"
-		scores[term] = int(score)  # Convert the score to an integer.
-
-	senti = calcScore("I am disappointed")
-
-	return HttpResponse(senti)
+	return render(request,'mainapp/index.html',)
