dc64cee18408e37dc2fc55b470ac8eea2f2ef3ca
Bidhan <bbidhan666@gmail.com>
2014-12-18 12:20:38 +0545

    senti on django

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import patterns, url
 from mainapp import views
 
 urlpatterns = patterns('',
+	url(r'^senti/$', views.senti, name='senti'),
 	url(r'^$', views.index, name='index'),
 )
