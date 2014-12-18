dbee46324197ad569528763b4ebddd05a35fca5c
Bidhan <bbidhan666@gmail.com>
2014-12-18 11:19:47 +0545

    added models

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+class Story(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    #user = models.ForeignKey(User)
+    sector = models.CharField(max_length=50)
+    status = models.CharField(max_length=50)
+    location = models.CharField(max_length=50)
+    senti = models.IntegerField() #average sentiment value of comments
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class Comment(models.Model):
+    description = models.TextField()
+    #user = models.ForeignKey(User)
+    story = models.ForeignKey(Story)
+    senti = models.IntegerField() #sentiment value
+    is_anonymous = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class Watch(models.Model):
+    story = models.ForeignKey(Story)
+    #user = models.ForeignKey(User)
+    created_at = models.DateTimeField(auto_now_add=True)
