dfd93855d1019b6561b8163ad102b57d6bb4d532
diwasblack <diwasblack@gmail.com>
2014-12-18 10:59:44 +0545

    Corrected structure

--- community_watch/settings.py
+++ community_watch/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'mainapp'
 )
 
 MIDDLEWARE_CLASSES = (
---
+++ mainapp/urls.py
@@ -0,0 +1,6 @@
+from django.conf.urls import patterns, url
+from mainapp import views
+
+urlpatterns = patterns('',
+url(r'^$', views.index, name='index'),
+)
